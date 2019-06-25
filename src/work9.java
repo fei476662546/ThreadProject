b18ab647c484dda119a580fa7cf260d81ea71b89
mysterioustime <fei476662546@gmail.com>
2019-06-25 21:24:12 +0800

    阿挺线程

---
+++ src/work9.java
@@ -0,0 +1,9 @@
+/**
+ * Create by mysteriousTime
+ * time on 2019/6/25  19:30
+ * 9.以下是一个模拟订票业务的程序。BookingClerk类代表自动售票员，
+ * 其中包含一个订票（booking）方法。假设一开始有10张票可预定。
+ * 程序运行时产生两个订票客户同时自动向自动售票员订票。请问会出现什么错误？该如何修改？
+ */
+public class work9 extends Thread{
+}
